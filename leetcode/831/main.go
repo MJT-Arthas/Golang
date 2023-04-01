814b9c3bd323d7f0fb74e176bd02f153bb8c0212
MJT-Arthas <787408654@qq.com>
2023-04-01 23:09:37 +0800

    gg

---
+++ leetcode/831/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+	"unicode"
+)
+
+func main() {
+	s := "LeetCode@LeetCode.com"
+
+	fmt.Println(maskPII(s))
+}
+
+func maskPII(s string) string {
+	at := strings.Index(s, "@")
+	if at > 0 {
+		s = strings.ToLower(s)
+		return strings.ToLower(string(s[0])) + "*****" + s[at-1:]
+	}
+	var sb strings.Builder
+	for i := 0; i < len(s); i++ {
+		c := s[i]
+		if unicode.IsDigit(rune(c)) {
+			sb.WriteByte(c)
+		}
+	}
+	s = sb.String()
+	country := []string{"", "+*-", "+**-", "+***-"}
+	return country[len(s)-10] + "***-***-" + s[len(s)-4:]
+}
